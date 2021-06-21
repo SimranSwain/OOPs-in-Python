d0a54676fba989916bcf78cae4cef1c9ee24a49a
SIMRAN SWAIN <61618523+SimranSwain@users.noreply.github.com>
2021-06-22 00:19:05 +0530

    Add files via upload

---
+++ Class_1.py
@@ -0,0 +1,35 @@
+# class Mobile():
+#     def __init__(self):
+#         self.Brandname="Samsung"
+#         self.color="Black"
+
+#     def calling(self):
+#         return("calling")
+
+#     def camera_click(self):
+#         return("click a picture")
+
+# m1=Mobile()
+# print(m1.Brandname)
+# print(m1.color)
+# print(m1.calling())
+
+class Mobile():
+    def __init__(self):
+        self.Brandname="Samsung"
+        self.color="Black"
+
+    def calling(self):
+        return("hii there")
+
+    def camera_click(self):
+        print("click a picture")
+
+m1=Mobile()
+m1.Brandname
+
+# print(m1.color)
+# print(m1.calling())
+m1.calling()
+
+
