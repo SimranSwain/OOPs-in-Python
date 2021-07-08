0b10fd1bdd31ce279a83ce0d8690abed5e58d0b4
SIMRAN SWAIN <swainsimran4@gmail.com>
2021-07-08 12:59:05 +0530

    Add files via upload

---
+++ Func_type_1.py
@@ -0,0 +1,22 @@
+# def addition():
+#     a,b=map(int,input().split())
+#     print(a+b)
+
+# def main():
+#      addition()
+
+
+# if __name__=='__main__':
+#    main()
+
+
+def addition():
+    a,b=map(int,input().split())
+    print(a+b)
+
+def main():
+     addition()
+
+
+if __name__=='__main__':
+   main()
