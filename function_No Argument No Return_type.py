9f64d1e29f906a0d620df576df292bb78f727e0f
SIMRAN SWAIN <swainsimran4@gmail.com>
2021-07-08 12:59:29 +0530

    Add files via upload

---
+++ function_No Argument No Return_type.py
@@ -0,0 +1,32 @@
+# def addition():
+#     a = int(input())
+#     b = int(input())
+#     print(a + b)
+#     # print(c+d)
+
+
+# def main():
+#     addition()
+
+
+# if __name__ == "__main__":
+#     main()
+
+
+#-------------------------------------------------------------------------------------
+
+
+def addition():
+    # a = int(input())
+    # b = int(input())
+    a,b=map(int,input().split())
+    print(a + b)
+    # print(c+d)
+
+
+def main():
+    addition()
+
+
+if __name__ == "__main__":
+    (main())
